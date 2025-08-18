227a56c04000b25b0af7226840dccecffa115a0e
RaihanKabir277 <raihank0198@gmail.com>
2025-08-18 08:06:19 +0600

    basic smtplib added here

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("day 32 starts here ........")
+# print("day 32 starts here ........")
+
+import smtplib
+
+my_mail = "-------@gmail.com"  #give your gmail here
+connection = smtplib.SMTP("smtp.gmail.com")
+# ------ transport layer security ------
+connection.starttls()
+connection.login(user=my_mail, password="********") #your password paste here
+connection.sendmail(from_addr=my_mail, to_addrs="----@yahoo.com", msg="Hello")
+connection.close()
+
 
