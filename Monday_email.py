2a69694ec11e513a5ed933b9d44c7872da777824
RaihanKabir277 <raihank0198@gmail.com>
2025-08-21 07:55:56 +0600

    monday motivational email add here

--- Monday_email.py
+++ Monday_email.py
@@ -2,6 +2,9 @@ import smtplib
 import datetime as dt
 import random
 
+my_email = "ra............"
+
+
 now = dt.datetime.now()
 weekday = now.weekday()
 
@@ -9,5 +12,9 @@ if weekday == 3:
     with open("quotes.txt") as quote_file:
         all_quotes = quote_file.readlines()
         quote = random.choice(all_quotes)
-    print(quote)
+    # 
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password="**************")
+        connection.sendmail(from_addr=my_email,to_addrs="----@yahoo.com", msg=f"Subject: Well wishers\n\n{quote}")
 
